b811ac1964af9c9b22b72301c59b20340f9b34c4
andrei110295@yandex.ru <andrei110295@yandex.ru>
2022-05-29 18:30:30 +0300

    Внес правки

--- gulpfile.js
+++ gulpfile.js
@@ -81,6 +81,7 @@ const copy = (done) => {
     'source/fonts/*.{woff2,woff}',
     'source/*.ico',
     'source/img/sprite.svg',
+    'source/manifest.webmanifest',
   ], {
     base: 'source'
   })
@@ -120,7 +121,7 @@ const reload = (done) => {
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/js/*.js', gulp.series(scripts));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
 // Build
